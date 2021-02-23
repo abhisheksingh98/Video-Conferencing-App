bafb43b298b2a5114082078b35c78013c9bf26a5
Abhishek Singh <writetoabhishek07@gmail.com>
2021-02-23 06:28:01 +0530

    update room view

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const peerServer = ExpressPeerServer(server, {
     debug : true
 })
 
+const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs',peerServer);
+
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.use('/peerjs',peerServer);
 
 app.get('/', (req,res)=>{
     res.redirect(`/${uuidv4()}`);
@@ -25,7 +28,16 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId,userId)=> {
         socket.join(roomId);
         socket.to(roomId).broadcast.emit('user-connected',userId);
+        socket.on('message', (message) => {
+            
+            io.to(roomId).emit('createMessage', message)
+        }); 
+      
+          socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+          })
+        
     })
 })
 
-server.listen(3030);
+server.listen(process.env.PORT||3030);
